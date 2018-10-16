6c1b6420a14e60d8a7245cf59c66f18dfdd04aa4
Carlos Andres Cadena Padilla <carlos.cadena@correounivalle.edu.co>
2018-10-15 20:37:21 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -25,7 +25,7 @@ if ( process.env.NODE_ENV === 'dev'){
 
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:azahella333@ds045507.mlab.com:45507/cafe-test';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
